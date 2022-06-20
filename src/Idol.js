060b90c4640812996f1e2b6a7e0a6c5e56573334
sjoleee <cent7425@gmail.com>
2022-06-20 23:26:26 +0900

    Idol 컴포넌트 생성

---
+++ src/Idol.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Idol() {
+  return (
+    <div>
+      <div>사진</div>
+      <div>이름</div>
+      <div>그룹명</div>
+    </div>
+  );
+}
+
+export default Idol();
