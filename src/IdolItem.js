35d59055352b870477e63644aa5d595963cc6dd7
sjoleee <cent7425@gmail.com>
2022-06-25 10:55:49 +0900

    각 idol 카드를 클릭하면 해당하는 id값이 저장되도록 useState를 사용

--- src/IdolItem.js
+++ src/IdolItem.js
@@ -1,9 +1,16 @@
 import "./IdolItem.css";
-import React from "react";
+import React, { useState } from "react";
 
 function IdolItem(props) {
+  let selectedItemArr = [];
+  const [selectedItem, setSelectedItem] = useState(selectedItemArr);
+  let selectedItemCopy = [...selectedItem];
+  selectedItemCopy.push(props.params.id);
+  setSelectedItem(selectedItemCopy);
+  console.log(selectedItem);
+
   return (
-    <div className="card">
+    <div className="card" onClick={() => {}}>
       <img src={props.params.src} className="card-img" />
       <div className="card-name">{props.params.name}</div>
       <div className="card-group">{props.params.group}</div>
--- src/data.js
+++ src/data.js
@@ -1,20 +1,20 @@
 export default [
   {
-    id: 123,
+    id: "123",
     name: "류현수",
     group: "kbank",
     src: "https://image.msscdn.net/images/goods_img/20220419/2499333/2499333_1_500.jpg",
   },
 
   {
-    id: 456,
+    id: "456",
     name: "송용호",
     group: "coinone",
     src: "https://image.msscdn.net/images/goods_img/20220419/2499333/2499333_1_500.jpg",
   },
 
   {
-    id: 789,
+    id: "789",
     name: "이상조",
     group: "inflab",
     src: "https://image.msscdn.net/images/goods_img/20220419/2499333/2499333_1_500.jpg",
