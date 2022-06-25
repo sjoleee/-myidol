56fbf0d0006c45034dc0380e7177a675f33e530b
sjoleee <cent7425@gmail.com>
2022-06-25 11:35:31 +0900

    idol별로 다른 state를 사용하게 되어, 같은 state를 공유하도록 수정

--- src/IdolItem.js
+++ src/IdolItem.js
@@ -2,15 +2,19 @@ import "./IdolItem.css";
 import React, { useState } from "react";
 
 function IdolItem(props) {
-  let selectedItemArr = [];
-  const [selectedItem, setSelectedItem] = useState(selectedItemArr);
-  let selectedItemCopy = [...selectedItem];
-  selectedItemCopy.push(props.params.id);
-  setSelectedItem(selectedItemCopy);
-  console.log(selectedItem);
+  const selectedItem = props.selectedItem;
+  const setSelectedItem = props.setSelectedItem;
 
   return (
-    <div className="card" onClick={() => {}}>
+    <div
+      className="card"
+      onClick={() => {
+        let selectedItemCopy = [...selectedItem];
+        selectedItemCopy.push(props.params.id);
+        setSelectedItem(selectedItemCopy);
+        console.log(selectedItem);
+      }}
+    >
       <img src={props.params.src} className="card-img" />
       <div className="card-name">{props.params.name}</div>
       <div className="card-group">{props.params.group}</div>
--- src/IdolItemList.js
+++ src/IdolItemList.js
@@ -5,10 +5,20 @@ import Data from "./data";
 
 function IdolItemList() {
   const [idol, setIdol] = useState(Data);
+
+  let selectedItemArr = [];
+  const [selectedItem, setSelectedItem] = useState(selectedItemArr);
+
   return (
     <div className="contaniner">
       {idol.map((params) => {
-        return <IdolItem params={params} />;
+        return (
+          <IdolItem
+            params={params}
+            selectedItem={selectedItem}
+            setSelectedItem={setSelectedItem}
+          />
+        );
       })}
     </div>
   );
